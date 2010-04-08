0b0fd3729a2d2ee6d592b09932fa89fa796fd2bd
conroy@google.com <conroy@google.com@01715948-b380-11de-8d12-5dda18c4c2b8>
2010-04-08 17:41:24 +0000

    Merge branch 'master' into local-breaky

--- src/client/ui/src/com/google/speedtracer/client/model/AggregateTimeVisitor.java
+++ src/client/ui/src/com/google/speedtracer/client/model/AggregateTimeVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Google Inc.
+ * Copyright 2010 Google Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,111 +13,50 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package com.google.speedtracer.client.model;
 
-import com.google.speedtracer.client.model.EventVisitor.PostOrderVisitor;
-import com.google.speedtracer.client.util.JSOArray;
+import com.google.gwt.core.client.JsArrayNumber;
 import com.google.speedtracer.client.util.JsIntegerDoubleMap;
 
 /**
- * Visitor for aggregating durations spent in each EventRecord Type in the
- * Context Tree for a UiEvent. This is responsible for computing self time and
- * for computing aggregate times spent in the various components of a node.
- * 
- * This must be run in a post order traversal.
+ * Computes self time and type durations for a {@link UiEvent}.
  */
-public class AggregateTimeVisitor implements PostOrderVisitor {
-
-  private boolean applied = false;
-  private final JsIntegerDoubleMap durations;
-
-  private final UiEvent rootEvent;
-
-  public AggregateTimeVisitor(UiEvent rootEvent) {
-    this.rootEvent = rootEvent;
-
-    JsIntegerDoubleMap durationMap = rootEvent.getTypeDurations();
-    if (durationMap != null) {
-      // We have already computed this.
-      durations = durationMap;
-      applied = true;
-    } else {
-      durations = JsIntegerDoubleMap.create();
+public class AggregateTimeVisitor {
+  private static class Visitor implements UiEvent.LeafFirstTraversalNumber {
+    private static void updateTypeDuration(JsIntegerDoubleMap typeDurations,
+        int type, double duration) {
+      typeDurations.put(type, typeDurations.hasKey(type)
+          ? typeDurations.get(type) + duration : duration);
     }
-  }
 
-  public boolean alreadyApplied() {
-    return applied;
-  }
+    private final JsIntegerDoubleMap typeDurations = JsIntegerDoubleMap.create();
 
-  /**
-   * Returns the time spend in the specified type, provided the durations map
-   * has been computed and initialized.
-   * 
-   * @return the time spent in the type
-   */
-  public final double getTimeSpentInType(int type) {
-    if (durations.hasKey(type)) {
-      return durations.get(type);
-    } else {
-      return 0;
-    }
-  }
-
-  /**
-   * Returns the Durations map, or null if this visitor has not been applied. We
-   * maintain an applied boolean instead of lazy initializing
-   * <code>durations</code> since we don't want to have to ensure that the
-   * durations map has been initialized each time we visit a UiEvent.
-   * 
-   * @return
-   */
-  public JsIntegerDoubleMap getTypeDurations() {
-    if (applied) {
-      return durations;
-    } else {
-      return null;
-    }
-  }
-
-  public void postProcess() {
-    if (applied) {
-      return;
-    } else {
-      applied = true;
-      rootEvent.setTypeDurations(durations);
-    }
-  }
-
-  public void visitUiEvent(UiEvent e) {
-    int currType = e.getType();
-    // Compute the self time of this node and update the time aggregates.
-    double duration = e.getDuration();
-    double childTime = computeTimeInChildren(e);
-    double selfTime = duration - childTime;
-    e.setSelfTime(selfTime);
-    updateDuration(currType, selfTime);
-  }
-
-  private double computeTimeInChildren(UiEvent node) {
-    double duration = 0;
-    JSOArray<UiEvent> children = node.getChildren();
-    for (int i = 0, n = children.size(); i < n; i++) {
-      UiEvent child = children.get(i);
-      if (child.getType() == EventRecordType.AGGREGATED_EVENTS) {
-        duration += child.getSelfTime();
-      } else {
-        duration += child.getDuration();
+    public double visit(UiEvent event, JsArrayNumber values) {
+      double childTime = 0;
+      for (int i = 0, n = values.length(); i < n; ++i) {
+        childTime += values.get(i);
       }
+      final double duration = event.getDuration();
+      final double selfTime = duration - childTime;
+      event.setSelfTime(selfTime);
+      updateTypeDuration(typeDurations, event.getType(), selfTime);
+      return duration;
     }
-    return duration;
   }
 
-  private void updateDuration(int type, double delta) {
-    double d = 0;
-    if (durations.hasKey(type)) {
-      d = durations.get(type);
+  /**
+   * Applies this visitor to the {@link UiEvent}. This will return immediately
+   * if this visitor was previously run on this event.
+   * 
+   * @param event
+   */
+  public static void apply(UiEvent event) {
+    if (event.getTypeDurations() != null) {
+      return;
     }
-    durations.put(type, d + delta);
+    final Visitor visitor = new Visitor();
+    event.apply(visitor);
+    event.setTypeDurations(visitor.typeDurations);
   }
 }
