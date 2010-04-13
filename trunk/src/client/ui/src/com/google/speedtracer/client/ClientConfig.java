be947502f7dc8322bb43b6df33a159245dc1c242
jaimeyap@google.com <jaimeyap@google.com@01715948-b380-11de-8d12-5dda18c4c2b8>
2010-04-13 20:42:02 +0000

    Version 0.13 bump
    R: knorton, zundel
    http://gwt-code-reviews.appspot.com/285802/show

--- trunk/src/client/ui/src/com/google/speedtracer/client/ClientConfig.java
+++ trunk/src/client/ui/src/com/google/speedtracer/client/ClientConfig.java
@@ -22,7 +22,7 @@ import com.google.gwt.core.client.GWT;
  * binding properties.
  */
 public class ClientConfig {
-  public static final String VERSION = "0.12";
+  public static final String VERSION = "0.13";
   
   @SuppressWarnings("unused")
   private static class DebugMode extends ReleaseMode {
