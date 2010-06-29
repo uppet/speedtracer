c5ebcfd96fa06dc1012e0fac8f56bf3be9263366
jaimeyap@google.com <jaimeyap@google.com@01715948-b380-11de-8d12-5dda18c4c2b8>
2010-06-29 21:42:07 +0000

    Fixes permission exceptions for attempting to fetch symbol servers on restricted urls.
    R: conroy
    http://gwt-code-reviews.appspot.com/651802/show

--- src/client/ui/src/com/google/speedtracer/client/ClientConfig.java
+++ src/client/ui/src/com/google/speedtracer/client/ClientConfig.java
@@ -22,7 +22,7 @@ import com.google.gwt.core.client.GWT;
  * binding properties.
  */
 public class ClientConfig {
-  public static final String VERSION = "0.16";
+  public static final String VERSION = "0.17";
   
   @SuppressWarnings("unused")
   private static class DebugMode extends ReleaseMode {
