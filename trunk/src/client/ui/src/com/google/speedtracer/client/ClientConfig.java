3980827e5ac6c5662a6360836b92915150d21567
conroy@google.com <conroy@google.com@01715948-b380-11de-8d12-5dda18c4c2b8>
2010-06-29 18:36:29 +0000

    Version bump to 0.16
    R:jaimeyap
    desk

--- trunk/src/client/ui/src/com/google/speedtracer/client/ClientConfig.java
+++ trunk/src/client/ui/src/com/google/speedtracer/client/ClientConfig.java
@@ -22,7 +22,7 @@ import com.google.gwt.core.client.GWT;
  * binding properties.
  */
 public class ClientConfig {
-  public static final String VERSION = "0.15";
+  public static final String VERSION = "0.16";
   
   @SuppressWarnings("unused")
   private static class DebugMode extends ReleaseMode {
