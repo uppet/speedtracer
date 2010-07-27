6da124c7a58258df35d84d40c92b345cc829c3b2
zundel@google.com <zundel@google.com@01715948-b380-11de-8d12-5dda18c4c2b8>
2010-07-27 23:12:54 +0000

    When working with a system that is pre-processing the speed trace data, he whitespace inside the <div> was being changed.  This parser change to the content script makes the JSON processing more resilient to whitespace (or lack thereof) between two json objects in the traceData element of the dump.
    R:conroy
    http://gwt-code-reviews.appspot.com/718801/show

--- trunk/src/client/ui/src/com/google/speedtracer/extension/public/data_loader.js
+++ trunk/src/client/ui/src/com/google/speedtracer/extension/public/data_loader.js
@@ -2,21 +2,92 @@ function isRecordDump() {
   return (document.documentElement.getAttribute("isDump") == "true");
 }
 
+
+var STATE_START = 0;
+var STATE_IN_JSO = 1;
+var STATE_IN_QUOTE = 2;
+
+// Examines the string starting at the specified offset to find complete
+// JSON object strings starting and ending with a curly brace: { ... }
+//   curr_offset - the character position to begin scanning the string
+//   data - the raw data string to split into separate jso strings
+// On success, returns an array:
+//   [ curr_offset, jso_string ]
+// If there is no more data in the string, returns null
+function nextJsonObject(curr_offset, data) {
+  var result = [];
+  var curr = curr_offset;
+  var curr_state = STATE_START;
+  var quote_type;
+  var curr_jso = [];
+  for (start = 0; curr < data.length ; curr++) {
+    var curr_char = data.charAt(curr);
+    curr_jso.push(curr_char);
+    
+    switch (curr_state) {
+    case STATE_START:
+      // drop any previously encountered characters preceding '{'
+      var nested_level = 0;
+      if (curr_char == "{") {
+        curr_state = STATE_IN_JSO;
+      } else {
+        curr_jso = [];
+      }
+    break;
+
+    case STATE_IN_JSO:
+      if (curr_char == "'") {
+        curr_state = STATE_IN_QUOTE;
+        quote_type = "'";
+      } else if (curr_char == '"') {
+        curr_state = STATE_IN_QUOTE;
+        quote_type = '"';
+      } else if (curr_char == '{') {
+        nested_level++;
+      } else if (curr_char == '}') {
+        if (nested_level < 0) {
+          throw new Error("Nested level should be > 0");
+        }
+        nested_level--;
+        if (nested_level == -1) {
+          return [curr + 1, curr_jso.join("")];
+        }
+      }
+    break;
+
+    case STATE_IN_QUOTE:
+      if (curr_char == "\\") {
+        curr++;
+        curr_char = data.charAt(curr);
+        curr_jso.push(curr_char);
+      } else if (curr_char == quote_type) {
+        curr_state = STATE_IN_JSO;
+      }
+    break;
+    }
+  }
+  // All valid records should have been spit out from STATE_IN_JSO
+  return null;
+}
+
 function sendData(port, dataContainer) {
   // 0.8 was the last version to not version saved files.
   var version = dataContainer.getAttribute("version") || "0.8";
   var allData = dataContainer.innerHTML;
-  for (var start = 0, end = allData.indexOf('\n', 0);
-       end != -1; end = allData.indexOf('\n', start)) {
-    var recordStr = allData.slice(start, end);
-    // Make sure the recordStr is not simply all whitespace.
+  
+  var json_result = [0]
+  while (true) {
+    var json_result = nextJsonObject(json_result[0], allData);
+    if (json_result == null) {
+      break;
+    }
+    recordStr = json_result[1];
     if (!/^\s*$/.test(recordStr)) {
       port.postMessage({
         version: version,
         record : recordStr
       });
     }
-    start = end + 1;
   }
   var info = document.getElementById("info");
   info.innerHTML = "(loading... complete!)";
