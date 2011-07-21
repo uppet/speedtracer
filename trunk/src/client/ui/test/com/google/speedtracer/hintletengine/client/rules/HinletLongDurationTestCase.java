93123d0653447333681878f4337724c406d2da95
haibinlu@google.com <haibinlu@google.com@01715948-b380-11de-8d12-5dda18c4c2b8>
2011-07-21 19:06:29 +0000

    Move hintlet test data to gwt code. Test cases are easier to create and maintain.
    R: sarahgsmit

---
+++ trunk/src/client/ui/test/com/google/speedtracer/hintletengine/client/rules/HinletLongDurationTestCase.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.speedtracer.hintletengine.client.rules;
+
+import com.google.gwt.junit.client.GWTTestCase;
+import com.google.speedtracer.hintletengine.client.rules.HintletLongDuration;
+
+/**
+ * Tests {@link HintletLongDuration}.
+ */
+public class HinletLongDurationTestCase extends GWTTestCase {
+
+  @Override
+  public String getModuleName() {
+    return "com.google.speedtracer.hintletengine.HintletEngineTest";
+  }
+
+  public void testNoHint() {
+    HintletTestHelper.runTest(new HintletLongDuration(), getTestCaseNoHint());
+  }
+
+  public void testWarningHint() {
+    HintletTestHelper.runTest(new HintletLongDuration(), getTestCaseWarningHint());
+  }
+
+  public void testCriticalHint() {
+    HintletTestHelper.runTest(new HintletLongDuration(), getTestCaseCriticalHint());
+  }
+
+  private native static HintletTestCase getTestCaseNoHint()/*-{
+    return {
+        "inputs" : [
+          {
+              "duration" : 0.316162109375,
+              "usedHeapSize" : 9427056,
+              "sequence" : 1,
+              "data" : {
+                  "x" : 419,
+                  "y" : 2,
+                  "height" : 22,
+                  "width" : 94
+              },
+              "time" : 1,
+              "type" : @com.google.speedtracer.shared.EventRecordType::PAINT_EVENT,
+              "children" : [
+
+              ],
+              "totalHeapSize" : 13131136
+          }
+        ],
+        "expectedHints" : [],
+        "description" : "Shouldn't trigger a rule - too short."
+    };
+  }-*/;
+
+  private native static HintletTestCase getTestCaseWarningHint()/*-{
+    return {
+        "inputs" : [
+          {
+              "duration" : 500.9,
+              "usedHeapSize" : 9427056,
+              "sequence" : 2,
+              "data" : {
+                  "x" : 419,
+                  "y" : 2,
+                  "height" : 22,
+                  "width" : 94
+              },
+              "time" : 2,
+              "type" : @com.google.speedtracer.shared.EventRecordType::PAINT_EVENT,
+              "children" : [
+
+              ],
+              "totalHeapSize" : 13131136
+          }
+        ],
+        "expectedHints" : [
+          {
+              "timestamp" : 2,
+              "hintletRule" : "Long Duration Events",
+              "refRecord" : 2,
+              "description" : "Event lasted: 500ms.  Exceeded threshold: 100ms",
+              "severity" : 3
+          }
+        ],
+        "description" : "Should trigger a rule w/ warning severity."
+    };
+  }-*/;
+
+  private native static HintletTestCase getTestCaseCriticalHint()/*-{
+    return {
+        "inputs" : [
+          {
+              "data" : {
+                  "x" : 419,
+                  "y" : 2,
+                  "width" : 94,
+                  "height" : 22
+              },
+              "children" : [],
+              "type" : @com.google.speedtracer.shared.EventRecordType::PAINT_EVENT,
+              "usedHeapSize" : 9427056,
+              "totalHeapSize" : 13131136,
+              "duration" : 3000.2,
+              "time" : 3,
+              "sequence" : 3
+
+          }
+        ],
+
+        "expectedHints" : [
+          {
+              "hintletRule" : "Long Duration Events",
+              "timestamp" : 3,
+              "description" : "Event lasted: 3000ms.  Exceeded threshold: 2000ms",
+              "refRecord" : 3,
+              "severity" : 2
+          }
+        ],
+        "description" : "Should trigger a critical alarm."
+    };
+  }-*/;
+}
---
+++ trunk/src/client/ui/test/com/google/speedtracer/hintletengine/client/rules/HintletTestHelper.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.speedtracer.hintletengine.client.rules;
+
+import com.google.gwt.coreext.client.JSOArray;
+import com.google.gwt.coreext.client.JSON;
+import com.google.gwt.coreext.client.JSON.JSONParseException;
+import com.google.gwt.junit.client.GWTTestCase;
+import com.google.speedtracer.breaky.client.DumpValidator;
+import com.google.speedtracer.breaky.client.JsonSchema.JsonSchemaResults;
+import com.google.speedtracer.client.model.EventRecord;
+import com.google.speedtracer.client.model.HintRecord;
+import com.google.speedtracer.hintletengine.client.HintletEventRecordProcessor;
+import com.google.speedtracer.hintletengine.client.HintletOnHintListener;
+import com.google.speedtracer.hintletengine.client.rules.HintletRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Helper for running hintlet test case. Validate input event using speedtracer JSON schema first.
+ * Match hints fired by hintlet rule against expected hints.
+ */
+public class HintletTestHelper {
+
+  private static DumpValidator validator;
+
+  private HintletTestHelper() {
+
+  }
+
+  private static class OnHintCallback implements HintletOnHintListener {
+
+    private List<HintRecord> savedHints = new ArrayList<HintRecord>();
+
+    public OnHintCallback() {
+    }
+
+    /**
+     * Save the incoming hint
+     */
+    public void onHint(String hintletRule, double timestamp, String description, int refRecord,
+        int severity) {
+      savedHints.add(HintRecord.create(hintletRule, timestamp, severity, description, refRecord));
+    }
+
+    public List<HintRecord> getHints() {
+      return savedHints;
+    }
+
+  }
+
+  public static void runTest(HintletRule rule, HintletTestCase testCase) {
+
+    OnHintCallback hintCallback = new OnHintCallback();
+    rule.setOnHintCallback(hintCallback);
+    HintletEventRecordProcessor eventRecordProcessor = new HintletEventRecordProcessor(rule);
+
+    validator = new DumpValidator();
+
+    JSOArray<EventRecord> inputs = testCase.getInputs();
+    for (int i = 0; i < inputs.size(); i++) {
+      EventRecord event = inputs.get(i);
+      // first validate using SpeedTracer JSON Schema
+      validateEventRecordFormat(event);
+      // then feed to even record processor
+      eventRecordProcessor.onEventRecord(event);
+    }
+
+    matchHints(testCase.getExpectedHints(), hintCallback.getHints());
+  }
+
+  private static void matchHints(JSOArray<HintRecord> expectedHints, List<HintRecord> hints) {
+
+    if ( expectedHints == null && hints.size() != 0) {
+      GWTTestCase.fail("Expecting zero hints. But got " + hints.size() + " hints." + "\nHints:\n"
+          + stringify(hints));
+    }
+
+    if (expectedHints.size() != hints.size()) {
+      GWTTestCase.fail("Expecting " + expectedHints.size() + " hints. But got " + hints.size()
+          + " hints." + "\nHints:\n" + stringify(hints) + "Expected Hints:\n"
+          + stringify(expectedHints));
+    }
+
+    for (int i = 0; i < expectedHints.size(); i++) {
+      if (!hintsMatch(expectedHints.get(i), hints.get(i))) {
+        GWTTestCase.fail("Expecting " + JSON.stringify(expectedHints.get(i)) + ". But got "
+            + JSON.stringify(hints.get(i)));
+      }
+    }
+  }
+
+  private static String stringify(JSOArray<HintRecord> expectedHints) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < expectedHints.size(); i++) {
+      sb.append("   " + ( i + 1) + ": ");
+      sb.append(JSON.stringify(expectedHints.get(i)));
+      sb.append("\n");
+    }
+    return sb.toString();
+  }
+
+  private static String stringify(List<HintRecord> hints) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < hints.size(); i++) {
+      sb.append("   " + ( i + 1) + ": ");
+      sb.append(JSON.stringify(hints.get(i)));
+      sb.append("\n");
+    }
+    return sb.toString();
+  }
+
+  private static void validateEventRecordFormat(EventRecord event) {
+    try {
+      JsonSchemaResults results = validator.validate(event);
+      GWTTestCase.assertTrue("Could not validate: \n" + JSON.stringify(event) + "\n"
+          + results.getErrors().get(0).getProperty() + " "
+          + results.getErrors().get(0).getMessage(), results.isValid());
+    } catch (JSONParseException e) {
+      GWTTestCase.fail("Got an exception trying to JSON parse the record: " + e.getMessage());
+    }
+  }
+
+  private static boolean hintsMatch(HintRecord hint1, HintRecord hint2) {
+    return (hint1.getDescription().equals(hint2.getDescription())
+        && hint1.getTimestamp() == hint2.getTimestamp() && hint1.getSeverity() == hint2
+        .getSeverity());
+  }
+}
