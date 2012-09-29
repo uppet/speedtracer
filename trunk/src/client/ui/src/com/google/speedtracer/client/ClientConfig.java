76b9d39cdb5f31105d560d4ae5ceae617858f9dc
jaimeyap@gmail.com <jaimeyap@gmail.com@01715948-b380-11de-8d12-5dda18c4c2b8>
2012-09-29 20:00:51 +0000

    Bumping version to 0.27 TBR: kellegous

--- trunk/src/client/ui/src/com/google/speedtracer/client/ClientConfig.java
+++ trunk/src/client/ui/src/com/google/speedtracer/client/ClientConfig.java
@@ -22,7 +22,7 @@ import com.google.gwt.core.client.GWT;
  * binding properties.
  */
 public class ClientConfig {
-  public static final String VERSION = "0.26";
+  public static final String VERSION = "0.27";
   
   @SuppressWarnings("unused")
   private static class DebugMode extends ReleaseMode {
